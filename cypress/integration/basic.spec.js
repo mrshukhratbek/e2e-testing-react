01ef5ae5e0faaf89ecc8e812ef0223cb593bc648
mrshuhrat <shuhratadm1n@gmail.com>
2022-01-09 16:29:18 +0500

    Write some test

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,6 +1,22 @@
+/// <reference types="Cypress" />
+
 describe('my first e2e test', () => {
-  it('actually works', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('render the site header', () => {
+    // cy.visit('/');
     cy.contains('Emoji Search');
+  });
+
+  it('render the list of emojis', () => {
+    // cy.visit('/');
+    // cy.get('.component-emoji-result-row').should('have.length', 20);
+    cy.get("[data-cy='emoji-row']").should('have.length', 20);
+  });
+
+  it('allows users to search for an emojis', () => {
+    cy.get('input').type('sun');
   });
 });
--- src/EmojiResultRow.js
+++ src/EmojiResultRow.js
@@ -1,11 +1,11 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import "./EmojiResultRow.css";
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import './EmojiResultRow.css';
 
 export default class EmojiResultsRow extends PureComponent {
   static propTypes = {
     title: PropTypes.string,
-    symbol: PropTypes.string
+    symbol: PropTypes.string,
   };
 
   render() {
@@ -14,6 +14,7 @@ export default class EmojiResultsRow extends PureComponent {
     return (
       <div
         className="component-emoji-result-row copy-to-clipboard"
+        data-cy="emoji-row"
         data-clipboard-text={this.props.symbol}
       >
         <img alt={this.props.title} src={src} />
